46abeb2bbd9030e8fe56532db5e2dd7bb8654512
bain-liao <liao.bain@mths.ca>
2024-11-07 10:54:49 -0500

    created program that uses radio to send signals if an object is too close

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Bain Liao
+ * Created on: Nov 2024
+ * This program determines if an object is too close using radios
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(177)
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Triangle)
+    //variables
+    let objectDistance: number = 0
+    // find object distance from sonar
+    objectDistance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    // if the object is <= 10
+    if (objectDistance <= 10) {
+        radio.sendString("Too Close!")
+    }
+    basic.showIcon(IconNames.Happy)
+})
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
