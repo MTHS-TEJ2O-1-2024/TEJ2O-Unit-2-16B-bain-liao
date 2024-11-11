4143b0faebb846d6f703f180746ec76c17d4e562
bain-liao <liao.bain@mths.ca>
2024-11-11 11:19:51 -0500

    removed the exclamation mark and fixed formatting

--- main.ts
+++ main.ts
@@ -11,17 +11,20 @@ basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Triangle)
+    
     //variables
     let objectDistance: number = 0
+
     // find object distance from sonar
     objectDistance = sonar.ping(
         DigitalPin.P1,
         DigitalPin.P2,
         PingUnit.Centimeters
     )
+    
     // if the object is <= 10
     if (objectDistance <= 10) {
-        radio.sendString("Too Close!")
+        radio.sendString("Too Close")
     }
     basic.showIcon(IconNames.Happy)
 })
